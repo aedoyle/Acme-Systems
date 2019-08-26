f6601010eca1f394be670f69d7d11c40d7a2fc0f
Alex Doyle <aedoylevsu@gmail.com>
2019-08-26 12:58:22 -0400

    Pushing a clone back to github

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -5,6 +5,7 @@ public class HelloDolly {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Dolly From Eclispe");
+		System.out.println("Hello Dolly From Eclispe again");
 	}
 
 }
